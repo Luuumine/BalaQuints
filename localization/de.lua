d3c9ab646f1c86ae2b7b87a882bdcd51d37b889b
Romain DELHOMMAIS <romain.delhommais@gmail.com>
2025-03-10 14:03:41 +0100

    german localization

---
+++ localization/de.lua
@@ -0,0 +1,34 @@
+-- de.lua
+return {
+    descriptions = {
+        alt_texture = {
+            alt_tex_quints_jokers = {
+                name = "Legendäre Joker",
+                j_caino = {name = "Ichika"},
+                j_yorick = {name = "Nino"},
+                j_triboulet = {name = "Miku"},
+                j_perkeo = {name = "Yotsuba"},
+                j_chicot = {name = "Itsuki"},
+            },
+
+            alt_tex_quints_spectrals = {
+                name = "Die Seele",
+                c_soul = {
+                    name = "Die Seele der Nakano",
+                },
+            },
+        },
+
+        texture_packs = {
+            texpack_quints_lumine_cards = {
+                name = "Lumine's Quintuplets",
+                text = {
+                    "Überarbeitet die Legendären Joker",
+                    "zu Charakteren aus",
+                    "Go-Toubun no Hanayome!",
+                    "{C:inactive,s:0.8}Art von Lumine und Turpix.{}",
+                },
+            },
+        },
+    },
+}
