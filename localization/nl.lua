f89d03f3640a748ec7b6c412e1e298c791af9f6e
Romain DELHOMMAIS <romain.delhommais@gmail.com>
2025-03-12 15:21:30 +0100

    dutch localization

---
+++ localization/nl.lua
@@ -0,0 +1,34 @@
+-- nl.lua
+return {
+    descriptions = {
+        alt_texture = {
+            alt_tex_quints_jokers = {
+                name = "Legendarische Jokers",
+                j_caino = {name = "Ichika"},
+                j_yorick = {name = "Nino"},
+                j_triboulet = {name = "Miku"},
+                j_perkeo = {name = "Yotsuba"},
+                j_chicot = {name = "Itsuki"},
+            },
+
+            alt_tex_quints_spectrals = {
+                name = "De ziel",
+                c_soul = {
+                    name = "Ziel van de Nakano",
+                },
+            },
+        },
+
+        texture_packs = {
+            texpack_quints_lumine_cards = {
+                name = "Lumine's Quintuplets",
+                text = {
+                    "Retexturiseert de Legendarische Jokers",
+                    "naar personages uit",
+                    "Go-Toubun no Hanayome!",
+                    "{C:inactive,s:0.8}Art door Lumine en Turpix.{}",
+                },
+            },
+        },
+    },
+}
