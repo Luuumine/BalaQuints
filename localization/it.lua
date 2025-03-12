71fd551fa1058b5dfafba20026f4026e423c40f2
Romain DELHOMMAIS <romain.delhommais@gmail.com>
2025-03-12 14:34:15 +0100

    italian localization

---
+++ localization/it.lua
@@ -0,0 +1,34 @@
+-- it.lua
+return {
+    descriptions = {
+        alt_texture = {
+            alt_tex_quints_jokers = {
+                name = "Joker Leggendari",
+                j_caino = {name = "Ichika"},
+                j_yorick = {name = "Nino"},
+                j_triboulet = {name = "Miku"},
+                j_perkeo = {name = "Yotsuba"},
+                j_chicot = {name = "Itsuki"},
+            },
+
+            alt_tex_quints_spectrals = {
+                name = "Anima",
+                c_soul = {
+                    name = "Anima delle Nakano",
+                },
+            },
+        },
+
+        texture_packs = {
+            texpack_quints_lumine_cards = {
+                name = "Lumine's Quintuplets",
+                text = {
+                    "Ritexturizza i Joker Leggendari",
+                    "con i personaggi di",
+                    "Go-Toubun no Hanayome!",
+                    "{C:inactive,s:0.8}Arte di Lumine e Turpix.{}",
+                },
+            },
+        },
+    },
+}
