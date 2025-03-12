21427bf7655897b700dda5d23e4d5b46c98cf772
Romain DELHOMMAIS <romain.delhommais@gmail.com>
2025-03-12 16:44:08 +0100

    brazilian localization

---
+++ localization/pt_BR.lua
@@ -0,0 +1,34 @@
+-- pt_BR.lua
+return {
+    descriptions = {
+        alt_texture = {
+            alt_tex_quints_jokers = {
+                name = "Coringas Lendários",
+                j_caino = {name = "Ichika"},
+                j_yorick = {name = "Nino"},
+                j_triboulet = {name = "Miku"},
+                j_perkeo = {name = "Yotsuba"},
+                j_chicot = {name = "Itsuki"},
+            },
+
+            alt_tex_quints_spectrals = {
+                name = "A alma",
+                c_soul = {
+                    name = "Alma das Nakano",
+                },
+            },
+        },
+
+        texture_packs = {
+            texpack_quints_lumine_cards = {
+                name = "Lumine's Quintuplets",
+                text = {
+                    "Retexturiza os Coringas Lendários",
+                    "em personagens de",
+                    "Go-Toubun no Hanayome!",
+                    "{C:inactive,s:0.8}Arte por Lumine e Turpix.{}",
+                },
+            },
+        },
+    },
+}
