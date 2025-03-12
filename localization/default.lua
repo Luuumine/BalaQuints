c9a998955651dff8984f53ee6c01100b13601db4
Romain DELHOMMAIS <romain.delhommais@gmail.com>
2025-03-12 13:28:33 +0100

    added support for localization

---
+++ localization/default.lua
@@ -0,0 +1,34 @@
+-- default.lua
+return {
+    descriptions = {
+        alt_texture = {
+            alt_tex_quints_jokers = {
+                name = "Legendary Jokers",
+                j_caino = {name = "Ichika"},
+                j_yorick = {name = "Nino"},
+                j_triboulet = {name = "Miku"},
+                j_perkeo = {name = "Yotsuba"},
+                j_chicot = {name = "Itsuki"},
+            },
+
+            alt_tex_quints_spectrals = {
+                name = "The Soul",
+                c_soul = {
+                    name = "The Nakano Soul",
+                },
+            },
+        },
+
+        texture_packs = {
+            texpack_quints_lumine_cards = {
+                name = "Lumine's Quintuplets",
+                text = {
+                    "Retextures legendary jokers",
+                    "to characters from",
+                    "Go-Toubun no Hanayome!",
+                    "{C:inactive,s:0.8}Art by Lumine and Turpix.{}",
+                },
+            },
+        },
+    },
+}
